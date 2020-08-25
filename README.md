27ee29e88b2a6c91c15665dde280508fb173357e
Lachezar <lachko35@abv.bg>
2020-08-26 00:05:47 +0300

    Added Users list and edit

--- README.md
+++ README.md
@@ -4,19 +4,22 @@ Project Assignment for PHP MVC Frameworks - Symfony - юли 2020
 
 =========
 
-
 # Introduction
-The purpose of the application is to allow Car Rental businesses to make reservation process for their customers. This application is based on Symfony and MySQL. UI is powered by Bootstrap.
+The purpose of the application is to allow Car Rental businesses to make reservation process for their customers. This application based on Symfony and MySQL. UI powered by Bootstrap.
 
 # Backend - Symfony
 ## Routes
 List of routes used by this application with limited access and grants.
  -User Group : User 
- - /profile - 
+ - /profile 
+ - /profile/edit 
  
 - User Group: Administrator
- - /admin/dashboard -
- - /admin/cars  -
+ - /admin/dashboard 
+ - /admin/cars  
+ - /admin/rent
+ - /admin/users
+ - /admin/contacts
   
 - Access: Public
   - / - View home page  
@@ -24,13 +27,14 @@ List of routes used by this application with limited access and grants.
   - /about - View to display about us information 
   - /register - View to display registration form
   - /login - View to display login form
- 
+  - /user/order/new/{id}
+  
   - /cars - View to display cars
   - /cars/{id} - View to display car with details
   
 ## Controllers
 Controllers used by this application.
-
+- OrdersController
 - CarController 
 - HomeController 
 - SecurityController 
@@ -48,20 +52,41 @@ Views used by this application.
 - register
 - login
 
--dashboard
-- admin/cars -
-- admin/cars/new -
-- admin/cars/edit -
-- admin/cars/id -
-- admin/cars/delete -
+- order/new/id
 
+-dashboard
+- admin/cars 
+- admin/cars/new 
+- admin/cars/edit 
+- admin/cars/id 
+- admin/cars/delete 
+
+- admin/users 
+- admin/users/new 
+- admin/users/edit 
+- admin/users/id 
+- admin/users/delete 
+
+- admin/rents 
+- admin/rents/new 
+- admin/rents/edit 
+- admin/rents/id 
+- admin/rents/delete 
+
+- admin/contact 
+- admin/contact/new 
+- admin/contact/edit 
+- admin/contact/id 
+- admin/contact/delete 
 
 # Database
 Database management system used by this app is MySQL.
 - Tables
     - user 
     - cars 
-    
+    - orders
+    - rents
+    -contact
 -seeds - data.sql
     - cars
     - users
@@ -70,24 +95,20 @@ Database management system used by this app is MySQL.
 ## Bootstrap
 For the better UI, this app uses Bootstrap CSS framework
 
-
-
-Installation and startup
+##Installation and startup
 ------------------
 
 To install applications, you must do the following commands:
-
-`` `Bash
 # clone the repository
 $ git cloning https://github.com/6alqlqlq/Car-Rental-Symfony
-$ cd CarRental
+$ cd Car-Rental-Symfony
 
 # identify dependent and then specify
 $ composer install
 
 # create a database, schema and load the test data
-$ php bin/console doctrine:database:creation
-$ php bin/console doctrine:schema:create
+$ php bin/console doctrine:database:create
+$ php bin/console doctrine:schema:update --force
 $ php bin/console doctrine:database:importing data.sql
 
 # start the web server
--- src/CarRentalBundle/Controller/UserController.php
+++ src/CarRentalBundle/Controller/UserController.php
@@ -7,7 +7,7 @@ use CarRentalBundle\Form\UserType;
 use CarRentalBundle\Service\Users\UserServiceInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -85,6 +85,141 @@ class UserController extends Controller
 
             ]
         );
+    }
+
+    /**
+     * Lists all user entities.
+     * @Security ("has_role('ROLE_ADMIN')")
+     * @Route("admin/users", name="user_index",methods={"GET"})
+     */
+
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('CarRentalBundle:User')->findAll();
+
+        return $this->render('users/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+    /**
+     * Creates a new car entity.
+     *
+     * @Route("admin/users/new", name="user_new",methods={"GET", "POST"})
+     * @Security ("has_role('ROLE_ADMIN')")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return RedirectResponse|Response|null
+     */
+    public function newAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm('CarRentalBundle\Form\UserType', $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('user_show', array('id' => $user->getId()));
+        }
+
+        return $this->render('users/new.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a car entity.
+     *
+     * @Route("admin/user/{id}", name="user_show",methods={"GET"})
+     * @Security ("has_role('ROLE_ADMIN')")
+     * @param User $user
+     * @return Response|null
+     */
+    public function showAction(User $user)
+    {
+        $deleteForm = $this->createDeleteForm($user);
+
+        return $this->render('users/show.html.twig', array(
+            'user' => $user,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing car entity.
+     *
+     * @Route("admin/user/{id}/edit", name="user_edit",methods={"GET", "POST"})
+     * @Security ("has_role('ROLE_ADMIN')")
+     * @param Request $request
+     * @param User $user
+     * @return RedirectResponse|Response|null
+     */
+    public function editAction(Request $request, User $user)
+    {
+        $deleteForm = $this->createDeleteForm($user);
+        $editForm = $this->createForm('CarRentalBundle\Form\UserType', $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+        }
+
+        return $this->render('users/user_edit.html.twig', array(
+            'user' => $user,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a car entity.
+     *
+     * @Route("admin/user/{id}", name="user_delete",methods={"DELETE"})
+     * @Security ("has_role('ROLE_ADMIN')")
+     * @param Request $request
+     * @param User $user
+     * @return RedirectResponse
+     */
+    public function deleteAction(Request $request, User $user)
+    {
+        $form = $this->createDeleteForm($user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('user_index');
+    }
+
+    /**
+     * Creates a form to delete a car entity.
+     * @Security ("has_role('ROLE_ADMIN')")
+     *
+     * @param User $user The car entity
+     *
+     * @return FormInterface The form
+     */
+    private function createDeleteForm(User $user)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('user_delete', array('id' => $user->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
     }
 
 
